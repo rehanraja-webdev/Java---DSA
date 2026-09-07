e3d27c46dcc5b5805b78f4dfb74b5caf60580b08
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-07 13:44:13 +0530

    Add TwoSumII class to find indices of two numbers that add up to a target

---
+++ leetcode problems/TwoSumII.java
@@ -0,0 +1,30 @@
+public class TwoSumII {
+  public static void main(String[] args) {
+    int[] arr = { 2, 7, 11, 15 };
+    int target = 22;
+
+    int[] res = twosumII(arr, target);
+
+    for (int num : res) {
+      System.out.print(num + " ");
+    }
+  }
+
+  static int[] twosumII(int[] arr, int target) {
+    int l = 0, r = arr.length - 1;
+
+    while (l < r) {
+      int sum = arr[l] + arr[r];
+
+      if (sum > target) {
+        r--;
+      } else if (sum < target) {
+        l++;
+      } else {
+        return new int[] { l, r };
+      }
+    }
+
+    return new int[] { -1, -1 };
+  }
+}
