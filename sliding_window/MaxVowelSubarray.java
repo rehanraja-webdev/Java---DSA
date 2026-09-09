c1cb9e8b821460baf0790719ef3c7418ba7dc804
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-09 10:28:45 +0530

    Add MaxVowelSubarray class to implement functionality for finding the subarray of size k with the maximum number of vowels

---
+++ sliding_window/MaxVowelSubarray.java
@@ -0,0 +1,32 @@
+package sliding_window;
+
+//Find the subarray of size k with maximum number of vowels
+public class MaxVowelSubarray {
+  public static void main(String[] args) {
+
+    String s = "abciiidef";
+    int k = 3;
+    System.out.println(maxVowelSubarray(s, k));
+  }
+
+  static int maxVowelSubarray(String s, int winSize) {
+    int maxVowel = 0;
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (isVowel(s.charAt(i)))
+        count++;
+
+      if (i >= winSize - 1) {
+        maxVowel = Math.max(maxVowel, count);
+        if (isVowel(s.charAt(i - winSize + 1))) {
+          count--;
+        }
+      }
+    }
+    return maxVowel;
+  }
+
+  static boolean isVowel(Character ch) {
+    return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+  }
+}
