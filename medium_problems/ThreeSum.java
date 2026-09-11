6bcb69f80c828d6ed8aa7ef01e70cda1de6e43c4
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-11 21:35:40 +0530

    Verify and solve ThreeSum with another TestCase

--- medium_problems/ThreeSum.java
+++ medium_problems/ThreeSum.java
@@ -52,7 +52,7 @@ public class ThreeSum {
   }
 
   public static void main(String[] args) {
-    int[] arr = { -1, 0, 1, 2, -1, -4 };
+    int[] arr = { 1, 2, 0, 1, 0, 0, 0, 0 };
 
     List<List<Integer>> result = threesum(arr);
 
