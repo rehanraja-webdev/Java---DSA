61a0284eedd75e37d708e7dab1fd8ab517a7df20
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-13 12:42:48 +0530

    Add RunningSum class to implement functionality for calculating running sums of an array

---
+++ subarray/RunningSum.java
@@ -0,0 +1,20 @@
+package subarray;
+
+public class RunningSum {
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 3, 4 };
+    int[] sum = runningSum(nums);
+    System.out.println(sum[3]);
+  }
+  
+  static int[] runningSum(int[] nums) {
+    int[] res = new int[nums.length];
+    int sum=0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      res[i] = sum;
+    }
+    
+    return res;
+  }
+}
