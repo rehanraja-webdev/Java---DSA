33c43b47042ad210ace7a08047a9b4848f9d6e10
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-13 12:35:41 +0530

    Add RangeSumII class to implement functionality for calculating range sums using prefix sums

---
+++ subarray/RangeSumII.java
@@ -0,0 +1,28 @@
+package subarray;
+
+public class RangeSumII {
+  static int rangeSum(int[] prefix, int left, int right) {
+    return prefix[right + 1] - prefix[left];
+  }
+
+  static int[] buildPrefixSum(int[] arr) {
+    int[] prefixSum = new int[arr.length + 1];
+    prefixSum[0] = 0;
+    int sum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+      prefixSum[i + 1] = sum;
+    }
+
+    return prefixSum;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 2, 4, 1, 5, 3 };
+
+    int[] prefix = buildPrefixSum(nums);
+
+    System.out.println(rangeSum(prefix, 1, 3));
+  }
+}
