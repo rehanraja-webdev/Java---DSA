7413e652b9a681dfa43778938f42fd294b3e160e
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-16 15:52:28 +0530

    Refactor RearrangeArray class and add optimal solution.

--- array/RearrangeArray.java
+++ array/RearrangeArray.java
@@ -12,12 +12,19 @@ public class RearrangeArray {
     for (int num : nums) {
       System.out.print(num + " ");
     }
+    System.out.println();
+
+    int[] res = rearrangeArrayOptimal(nums);
+
+    for (int num : res) {
+      System.out.print(num + " ");
+    }
   }
 
+  // brute force approach
   static void rearrangeArray(int[] nums) {
     List<Integer> posList = new ArrayList<>();
     List<Integer> negList = new ArrayList<>();
-
     for (int i = 0; i < nums.length; i++) {
       if (nums[i] >= 0) {
         posList.add(nums[i]);
@@ -26,9 +33,27 @@ public class RearrangeArray {
       }
     }
 
-    for (int i = 0; i < nums.length/2; i++) {
-      nums[i*2] = posList.get(i);
-      nums[i*2+1] = negList.get(i);
+    for (int i = 0; i < nums.length / 2; i++) {
+      nums[i * 2] = posList.get(i);
+      nums[i * 2 + 1] = negList.get(i);
     }
+  }
+
+  static int[] rearrangeArrayOptimal(int[] nums) {
+    int[] res = new int[nums.length];
+
+    int pIdx = 0;
+    int nIdx = 1;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] < 0) {
+        res[nIdx] = nums[i];
+        nIdx += 2;
+      } else {
+        res[pIdx] = nums[i];
+        pIdx += 2;
+      }
+    }
+
+    return res;
   }
 }
