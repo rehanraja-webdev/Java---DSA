47c3a29d78a9d5750be71407bd03d922cf6964a6
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-20 17:31:44 +0530

    Add longestConsecutiveOptimal method to find the longest consecutive sequence in O(N) time complexity

--- array/LongestConsecutiveSequence.java
+++ array/LongestConsecutiveSequence.java
@@ -1,13 +1,16 @@
 package array;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class LongestConsecutiveSequence {
   public static void main(String[] args) {
     int[] nums = { 4, 2, 6, 5, 100, 2, 100, 1, 1, 102, 3, 101 };
     System.out.println(longestConsecutiveSequence(nums));
+    System.out.println(longestConsecutiveOptimal(nums));
   }
 
+  // Time complexity will be O(NlogN), because we are sorting the array first
   static int longestConsecutiveSequence(int[] nums) {
     int longest = 1;
     int count = 1;
@@ -23,6 +26,29 @@ public class LongestConsecutiveSequence {
       }
       num = nums[i];
       longest = Math.max(longest, count);
+    }
+    return longest;
+  }
+
+  // Time and space complexity O(logN)
+  static int longestConsecutiveOptimal(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+    for (int num : nums) {
+      set.add(num);
+    }
+
+    int longest = 0;
+    for (int num : set) {
+      if (!set.contains(num - 1)) {
+        int count = 1;
+        int curNum = num;
+
+        while (set.contains(curNum + 1)) {
+          count++;
+          curNum++;
+        }
+        longest = Math.max(longest, count);
+      }
     }
     return longest;
   }
